e0042676547ea2a8eed2af1b7a1df6b80c1a15f2
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-08-19 16:16:20 +0100

    Fourth Commit

--- src/main/java/com/cavsteek/CrudRestApi/Controller/ApiControllers.java
+++ src/main/java/com/cavsteek/CrudRestApi/Controller/ApiControllers.java
@@ -4,6 +4,8 @@ import com.cavsteek.CrudRestApi.Model.User;
 import com.cavsteek.CrudRestApi.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +23,12 @@ public class ApiControllers {
     @GetMapping("/users")
     public List<User> getUsers(){
         return userRepo.findAll();
+    }
+
+    @PostMapping("/save")
+    public String saveUser (@RequestBody User user){
+        userRepo.save(user);
+        return "Done.....";
     }
 
 
